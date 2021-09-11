757c17499542121413be7d57dd6466176e1f7330
exauce-mwililikwa <exaucemwililikwa@gmail.com>
2021-09-11 11:28:28 +0200

    updating

---
+++ gestionmateriel/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace gestionmateriel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SEXE = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PRENOM = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.POSTNOM = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NOM = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MATRICULE = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.SEXE);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.PRENOM);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.POSTNOM);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.NOM);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.MATRICULE);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-4, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(569, 519);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(285, 461);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 14;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(117, 371);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "PHOTO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "SEXE";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(117, 291);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(273, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // SEXE
+            // 
+            this.SEXE.FormattingEnabled = true;
+            this.SEXE.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.SEXE.Location = new System.Drawing.Point(117, 228);
+            this.SEXE.Name = "SEXE";
+            this.SEXE.Size = new System.Drawing.Size(273, 21);
+            this.SEXE.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SEXE";
+            // 
+            // PRENOM
+            // 
+            this.PRENOM.Location = new System.Drawing.Point(117, 177);
+            this.PRENOM.Name = "PRENOM";
+            this.PRENOM.Size = new System.Drawing.Size(273, 20);
+            this.PRENOM.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "PRENOM";
+            // 
+            // POSTNOM
+            // 
+            this.POSTNOM.Location = new System.Drawing.Point(117, 117);
+            this.POSTNOM.Name = "POSTNOM";
+            this.POSTNOM.Size = new System.Drawing.Size(273, 20);
+            this.POSTNOM.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "POSTNOM";
+            // 
+            // NOM
+            // 
+            this.NOM.Location = new System.Drawing.Point(117, 66);
+            this.NOM.Name = "NOM";
+            this.NOM.Size = new System.Drawing.Size(273, 20);
+            this.NOM.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NOM";
+            // 
+            // MATRICULE
+            // 
+            this.MATRICULE.Location = new System.Drawing.Point(117, 15);
+            this.MATRICULE.Name = "MATRICULE";
+            this.MATRICULE.Size = new System.Drawing.Size(273, 20);
+            this.MATRICULE.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MATRICULE";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(658, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(315, 291);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(53, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(211, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SUPPRIMER";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MODIFIER";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ENREGISTRER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 545);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(982, 159);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(807, 514);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 702);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "=";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox SEXE;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PRENOM;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox POSTNOM;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NOM;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MATRICULE;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ gestionmateriel/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace gestionmateriel
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        //executer les requete a distance
+        SqlCommand cmd;
+        public static string imgLon = "";
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public void chargement()
+        {
+            try {
+                //ouvrons notre connexion
+                con.Open();
+                cmd = con.CreateCommand();
+                //creons une instance de la classe etudiant
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM ETUDIANT",con);
+                //gardons les donnees en memoire
+                DataSet ds = new DataSet();
+                da.Fill(ds, "ETUDIANT");
+                //chargeons dans un control appeler datagridview
+                dataGridView1.DataSource = ds.Tables[0];
+                
+                con.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("erreur : "+ex);
+            }
+        }
+        public void RECHERCHERt()
+        {
+            try
+            {
+                //ouvrons notre connexion
+                con.Open();
+                cmd = con.CreateCommand();
+                //creons une instance de la classe etudiant
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM ETUDIANT WHERE CONCAT(NOM,' ',POSTNOM,' ',PRENOM) LIKE '%"+textBox1.Text+"%'", con);
+                //gardons les donnees en memoire
+                DataSet ds = new DataSet();
+                da.Fill(ds, "ETUDIANT");
+                //chargeons dans un control appeler datagridview
+                dataGridView1.DataSource = ds.Tables[0];
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("erreur : " + ex);
+            }
+        }
+        public void inserer()
+        {
+            try {
+                //transformons notre photo en tableau de byte
+                MemoryStream ms = new MemoryStream();
+                pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] photo = new Byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(photo, 0, photo.Length);
+                //ouvrons notre connection
+                con.Open();
+                //executons une requette sur le serveur
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                //requette sql
+                cmd.CommandText = "INSERT INTO ETUDIANT (NOM,POSTNOM,PRENOM,GENRE,DATENAISSANCE,PHOTO) VALUES(@NOM,@POSTNOM,@PRENOM,@GENRE,@DATENAISSANCE,@PHOTO)";
+                cmd.Parameters.AddWithValue("@nom", NOM.Text);
+                cmd.Parameters.AddWithValue("@postnom", POSTNOM.Text);
+                cmd.Parameters.AddWithValue("@PRENOM",PRENOM.Text);
+                cmd.Parameters.AddWithValue("@GENRE", SEXE.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@DATENAISSANCE", dateTimePicker1.Value.ToString());
+                cmd.Parameters.AddWithValue("@PHOTO", photo);
+                //executons la requete
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("insertion reussi");
+                //actualisons notre DataGridView
+                chargement();
+               
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(""+ex);
+            }
+        }
+        public void modifier()
+        {
+            try
+            {
+                //transformons notre photo en tableau de byte
+                MemoryStream ms = new MemoryStream();
+                pictureBox1.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] photo = new Byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(photo, 0, photo.Length);
+                //ouvrons notre connection
+                con.Open();
+                //executons une requette sur le serveur
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                //requette sql
+                cmd.CommandText = "update ETUDIANT set NOM=@NOM,POSTNOM=@POSTNOM,PRENOM=@PRENOM,GENRE=@GENRE,DATENAISSANCE=@DATENAISSANCE,PHOTO=@PHOTO where MATRICULE=@MATRICULE";
+                cmd.Parameters.AddWithValue("@nom", NOM.Text);
+                cmd.Parameters.AddWithValue("@postnom", POSTNOM.Text);
+                cmd.Parameters.AddWithValue("@PRENOM", PRENOM.Text);
+                cmd.Parameters.AddWithValue("@GENRE", SEXE.SelectedItem.ToString());
+                cmd.Parameters.AddWithValue("@DATENAISSANCE", dateTimePicker1.Value.ToString());
+                cmd.Parameters.AddWithValue("@PHOTO", photo);
+                cmd.Parameters.AddWithValue("@MATRICULE", int.Parse(MATRICULE.Text));
+                //executons la requete
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("MODIFICATION reussi");
+                //actualisons notre DataGridView
+                chargement();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+                String chemin_d_acces = string.Format(@"Data Source=EXAUCE\SA;Initial Catalog=GESTIONMATERIELS;User ID=sa;Password=aliconnorecho");
+                con = new SqlConnection(chemin_d_acces);
+                chargement();
+                // this.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("mamaeeeeeeee!"+ex.Message);
+            } 
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inserer();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {  
+            try {
+                OpenFileDialog dl = new OpenFileDialog();
+              dl.InitialDirectory=@"C:\Users\EXAUCE\Pictures";
+                dl.FilterIndex = 2;
+                dl.Filter = "JPEG |*.jpg";
+                if (dl.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(dl.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(""+ex);
+            }
+           
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                MATRICULE.Text = row.Cells[0].Value.ToString();
+                NOM.Text = row.Cells[1].Value.ToString();
+                POSTNOM.Text = row.Cells[2].Value.ToString();
+                PRENOM.Text = row.Cells[3].Value.ToString();
+                SEXE.SelectedItem = row.Cells[4].Value.ToString();
+                // PRENOM.Text = row.Cells[3].Value.ToString();
+               dateTimePicker1.Text = row.Cells[5].Value.ToString();
+                MemoryStream ms = new MemoryStream();
+
+                byte[] picture = (byte[])row.Cells[6].Value;
+                ms.Write(picture, 0, picture.Length);
+                pictureBox1.Image = new System.Drawing.Bitmap(ms);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            modifier();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            RECHERCHERt();    
+        }
+    }
+}
